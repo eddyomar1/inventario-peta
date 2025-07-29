5bbbd8b744535dff43be12b46c1f7172e0f83900
EO <eo@kali>
2025-07-29 10:40:48 -0400

    version 0.4

--- crud.php
+++ crud.php
@@ -17,7 +17,7 @@ $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
 
 // 3) Leer todos los registros
 if ($action === 'read') {
-    $res = $con->query("SELECT * FROM standartCrud ORDER BY id DESC");
+    $res = $con->query("SELECT * FROM standartCrud ORDER BY nombre DESC");
     $data = [];
     while ($row = $res->fetch_assoc()) {
         $data[] = $row;
